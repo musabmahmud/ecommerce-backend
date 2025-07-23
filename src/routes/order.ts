44d0a718f43a8b1470b0a0c83513e445e75a3ffb
md.musabmahmud100@gmail.com <md.musabmahmud100@gmail.com>
2025-07-23 20:17:47 +0600

    completed order

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -8,6 +8,7 @@ const router = express.Router();
 
 // Create Order
 router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
+
     try {
         // 1. Create address first
         const newAddress = new Address({
@@ -41,7 +42,7 @@ router.post('/create', async (req: Request, res: Response, next: NextFunction) =
         if (!savedOrder) {
             return res.status(400).json({ message: "Failed to create order" });
         }
-        
+
         // 3. Populate and return the complete order
         const completeOrder = await Order.findById(savedOrder._id)
             .populate('address')
@@ -57,4 +58,59 @@ router.post('/create', async (req: Request, res: Response, next: NextFunction) =
     }
 })
 
+//cancel order
+
+router.delete('/cancel/:id', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const orderId = req.params.id;
+        if (!orderId) {
+            return res.status(400).json({ message: "Order ID is required" });
+        }
+        const order = await Order.findById(orderId);
+        if (!order) {
+            return res.status(404).json({ message: "Order not found" });
+        }
+        if (order.status === 'cancelled') {
+            return res.status(400).json({ message: "Order is already cancelled" });
+        }
+        order.status = 'cancelled';
+        const updatedOrder = await order.save();
+        if (!updatedOrder) {
+            return res.status(400).json({ message: "Failed to cancel order" });
+        }
+        res.status(200).json({
+            message: "Order cancelled successfully",
+            data: updatedOrder
+        })
+    }
+    catch (error) {
+        next(error);
+    }
+})
+
+//single order
+
+
+router.get('/:userId/:orderId', async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.params.userId;
+    const orderId = req.params.orderId;
+    if (!userId) {
+        return res.status(400).json({ message: "User ID is required" });
+    }
+    try {
+        const order = await Order.find({ user: userId, _id: orderId }).populate('address').populate('products.product');
+        if (!order) {
+            return res.status(404).json({
+                message: "Order not found"
+            })
+        }
+        return res.status(200).json({
+            message: "Order found",
+            data: order
+        });
+    }
+    catch (error) {
+        next(error);
+    }
+})
 export default router;
